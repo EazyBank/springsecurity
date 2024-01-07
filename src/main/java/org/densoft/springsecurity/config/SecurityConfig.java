34d6b91ba812df38b25b97e27f567f037b8b62ae
dennis githinji <wakahiad@gmail.com>
2024-01-07 14:46:15 +0300

    spring security in-memmory database

--- src/main/java/org/densoft/springsecurity/config/SecurityConfig.java
+++ src/main/java/org/densoft/springsecurity/config/SecurityConfig.java
@@ -4,17 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("12345")).authorities("admin").build();
+
+        UserDetails user = User.withUsername("user").password(passwordEncoder().encode("12345")).authorities("read").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(registry -> registry
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact").permitAll()
-        );
+        http.authorizeHttpRequests(registry -> registry.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated().requestMatchers("/notices", "/contact").permitAll());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
