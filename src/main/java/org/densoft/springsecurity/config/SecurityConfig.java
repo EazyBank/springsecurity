aec8799123ec9e17b3a65919893e1341c6967970
dennis githinji <wakahiad@gmail.com>
2024-01-07 17:00:30 +0300

    code cleanup

--- src/main/java/org/densoft/springsecurity/config/SecurityConfig.java
+++ src/main/java/org/densoft/springsecurity/config/SecurityConfig.java
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,23 +18,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//
-//        UserDetails admin = User
-//                .withUsername("admin")
-//                .password(passwordEncoder().encode("12345"))
-//                .authorities("admin")
-//                .build();
-//
-//        UserDetails user = User
-//                .withUsername("user")
-//                .password(passwordEncoder().encode("12345"))
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
 
     @Bean
     public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
